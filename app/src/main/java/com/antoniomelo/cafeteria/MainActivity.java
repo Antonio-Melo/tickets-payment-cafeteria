1e98fbd616633e06aa58da15192a118ea8b08960
Antonio-Melo <antonio.aptmelo@gmail.com>
2018-11-18 17:15:06 +0000

    Create transaction done

--- app/src/main/java/com/antoniomelo/cafeteria/MainActivity.java
+++ app/src/main/java/com/antoniomelo/cafeteria/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter adapter;
     private ArrayList<ListOrder> orders;
     public Boolean validadeVouchersStatus = false;
+    public Boolean createTransactionStatus = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +43,8 @@ public class MainActivity extends AppCompatActivity {
         adapter = new OrdersAdaptor(orders, this);
         recyclerView.setAdapter(adapter);
 
-        System.out.println(validateVouchers());
+        //System.out.println(validateVouchers());
+        System.out.println(createTransaction());
     }
 
     private Boolean validateVouchers(){
@@ -52,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
             body.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
 
             AsyncHttpClient client = new AsyncHttpClient();
-            final Boolean status = false;
 
             client.post(null,"http://10.0.2.2:3000/validation/vouchers", body, "application/json", new AsyncHttpResponseHandler() {
 
@@ -76,5 +77,47 @@ public class MainActivity extends AppCompatActivity {
             return validadeVouchersStatus;
         }
         return validadeVouchersStatus;
+    }
+
+    private Boolean createTransaction() {
+        try {
+            String bodyString = "{\n" +
+                    "  \"uuid\": \"3309fb76-107f-4f7d-aeaa-afc3cbaaf8df\",\n" +
+                    "  \"order\": {\n" +
+                    "    \"coffee\": \"2\",\n" +
+                    "    \"drink\": \"3\",\n" +
+                    "    \"popcorn\": \"1\",\n" +
+                    "    \"sandwich\": \"1\"\n" +
+                    "  },\n" +
+                    "  \"vouchers\": [\"fcac5224-4890-4e14-98a1-024df8ff88e7\", \"3e257e02-790b-4fe0-8fbe-1f02f2ee7e95\"]\n" +
+                    "}";
+
+            StringEntity body = new StringEntity(bodyString);
+            body.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+
+            AsyncHttpClient client = new AsyncHttpClient();
+
+            client.post(null,"http://10.0.2.2:3000/users/order", body, "application/json", new AsyncHttpResponseHandler() {
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    System.out.println("STATUS CODE: " +statusCode);
+                    createTransactionStatus = true;
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    System.out.println("STATUS CODE: " +statusCode);
+                    System.out.println(error);
+                    createTransactionStatus = false;
+                }
+            });
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            validadeVouchersStatus = false;
+            return createTransactionStatus;
+        }
+        return createTransactionStatus;
     }
 }
